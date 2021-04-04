3172d4dce1679af497d1d80874ccd5a097ec84e0
Yukitaka Ohmura <ohmurayu@amazon.co.jp>
2021-04-04 19:22:05 +0900

    Remove unused imports

--- bin/able-ct-guest-base.ts
+++ bin/able-ct-guest-base.ts
@@ -1,29 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
 import { ABLEIamStack } from '../lib/able-iam-stack';
-import { ABLEASGAppStack } from '../lib/able-asgapp-stack';
-import { ABLEEC2AppStack } from '../lib/able-ec2app-stack';
 import { ABLEConfigRulesStack } from '../lib/able-config-rules-stack';
-import { ABLEConfigCtGuardrailStack } from '../lib/able-config-ct-guardrail-stack';
-import { ABLEGuarddutyStack } from '../lib/able-guardduty-stack';
-import { ABLETrailStack } from '../lib/able-trail-stack';
-import { ABLEVpcStack } from '../lib/able-vpc-stack';
-import { ABLEFlowLogKeyStack } from '../lib/able-flowlog-key-stack';
-import { ABLEFlowLogStack } from '../lib/able-flowlog-stack';
-import { ABLEGeneralLogKeyStack } from '../lib/able-generallog-key-stack';
-import { ABLEGeneralLogStack } from '../lib/able-generallog-stack';
-import { ABLEDbAuroraPgStack } from '../lib/able-db-aurora-pg-stack';
-import { ABLESecurityHubStack } from '../lib/able-security-hub-stack';
-import { ABLEConfigStack } from '../lib/able-config-stack';
-import { ABLEECSAppStack } from '../lib/able-ecsapp-stack';
-import { ABLEDbAuroraPgSlStack } from '../lib/able-db-aurora-pg-sl-stack';
-import { ABLEMonitorAlarmStack } from '../lib/able-monitor-alarm-stack';
-import { ABLEInvestigationInstanceStack } from '../lib/able-investigation-instance-stack';
 import { ABLESecurityAlarmStack } from '../lib/able-security-alarm-stack';
 import { ABLEChatbotStack } from '../lib/able-chatbot-stack';
-
-import { ABLEBuildContainerStack } from '../lib/able-build-container-stack';
-import { ABLEECRStack } from '../lib/able-ecr-stack';
 
 const procEnv = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
