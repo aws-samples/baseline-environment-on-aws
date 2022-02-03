1f2a11d3655c13d2da236894e08b9a69173be7ae
Wataru Mikuriya <wmikuriy@amazon.com>
2022-02-03 16:26:29 +0900

    disable self pipeline update

--- usecases/guest-webapp-sample/lib/blea-ecsapp-sample-pipeline-stack.ts
+++ usecases/guest-webapp-sample/lib/blea-ecsapp-sample-pipeline-stack.ts
@@ -34,6 +34,7 @@ export class BLEAPipelineStack extends cdk.Stack {
     // You just have to select GitHub as the source when creating the connection in the console
     // basic pipeline declaration. This sets the initial structure of our pipeline
     const pipeline = new pipelines.CodePipeline(this, 'pipeline', {
+      selfMutation: false,
       pipelineName: 'EcsSamplePipeline',
       // ここをShellStageにするのか、CodeBuildStageにするのかは、コマンドをCodeBuildで走らせたいかに依存する。
       synth: new pipelines.CodeBuildStep('SynthStep', {
