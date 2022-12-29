50ae15ad20a269fd28bb86e08c1db4a1ac82cf21
Kenji Kono <konoken@amazon.co.jp>
2022-12-29 17:36:45 +0900

    fix(base-standalone): remove chatbot params

--- usecases/blea-base-standalone/parameter.ts
+++ usecases/blea-base-standalone/parameter.ts
@@ -4,14 +4,10 @@ export interface MyParameter {
   env?: Environment;
   envName: string;
   securityNotifyEmail: string;
-  chatbotSlackWorkspaceId: string;
-  chatbotSlackChannelId: string;
 }
 
 // Example
 export const DevParameter: MyParameter = {
   envName: 'Development',
   securityNotifyEmail: 'notify-security@example.com',
-  chatbotSlackWorkspaceId: 'T8XXXXXXX',
-  chatbotSlackChannelId: 'C00XXXXXXXX',
 };
