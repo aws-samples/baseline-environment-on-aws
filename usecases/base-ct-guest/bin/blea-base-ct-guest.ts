cf1222aa75e21d016b00372aa0b7c6212e32f104
Kenji Kono <konoken@amazon.co.jp>
2023-01-06 13:22:36 +0900

    feat(base-ct-guest)!: compose into 1 stack and use parameter.ts

--- usecases/base-ct-guest/bin/blea-base-ct-guest.ts
+++ usecases/base-ct-guest/bin/blea-base-ct-guest.ts
@@ -1,73 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import { BLEAIamStack } from '../lib/blea-iam-stack';
-import { BLEAConfigRulesStack } from '../lib/blea-config-rules-stack';
-import { BLEASecurityAlarmStack } from '../lib/blea-security-alarm-stack';
-import { BLEAChatbotStack } from '../lib/blea-chatbot-stack';
-import { BLEATrailStack } from '../lib/blea-trail-stack';
+import { BLEABaseCTGuestStack } from '../lib/stack/blea-base-ct-guest-stack';
 
-const pjPrefix = 'BLEA-BASE';
+// Import parameters for each enviroment
+import { DevParameter } from '../parameter';
 
 const app = new cdk.App();
 
-// ----------------------- Load context variables ------------------------------
-// This context need to be specified in args
-const argContext = 'environment';
-const envKey = app.node.tryGetContext(argContext);
-if (envKey == undefined)
-  throw new Error(`Please specify environment with context option. ex) cdk deploy -c ${argContext}=dev`);
-
-const envVals = app.node.tryGetContext(envKey);
-if (envVals == undefined) throw new Error('Invalid environment.');
-
-// ----------------------- Environment variables for stack ------------------------------
-// Default enviroment
-const procEnvDefault = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
-
-// Define account id and region from context.
-// If "env" isn't defined on the environment variable in context, use account and region specified by "--profile".
-function getProcEnv() {
-  if (envVals['env'] && envVals['env']['account'] && envVals['env']['region']) {
-    return { account: envVals['env']['account'], region: envVals['env']['region'] };
-  } else {
-    return procEnvDefault;
-  }
-}
-
-// ----------------------- Guest Account Base Stacks ------------------------------
-new BLEAConfigRulesStack(app, `${pjPrefix}-ConfigRule`, { env: getProcEnv() });
-new BLEAIamStack(app, `${pjPrefix}-Iam`, { env: getProcEnv() });
-
-// AWS CloudTrail configuration in Control Tower Landing Zone v3.0 will not create CloudWatch Logs LogGroup in each Guest Accounts.
-// And it will delete these LogGroups when AWS CloudTrial Configuration is disabled in case of updating Landing Zone version from older one.
-// BLEA should notify their alarms continuously. So, if there is no CloudTrail and CloudWatch Logs in Guest Account, BLEA creates them to notify the Alarms.
-
-const trail = new BLEATrailStack(app, `${pjPrefix}-Trail`, { env: getProcEnv() });
-const logGroupName = trail.cloudTrailLogGroup.logGroupName;
-
-// Security Alarms
-// !!! Need to setup SecurityHub, GuardDuty manually on Organizations Management account
-// AWS Config and CloudTrail are set up by ControlTower
-const secAlarm = new BLEASecurityAlarmStack(app, `${pjPrefix}-SecurityAlarm`, {
-  notifyEmail: envVals['securityNotifyEmail'],
-  cloudTrailLogGroupName: logGroupName,
-  env: getProcEnv(),
+// Create stack for "Dev" environment.
+// If you have multiple environments, instantiate stacks with its parameters.
+new BLEABaseCTGuestStack(app, 'DevBLEABaseCTGuest', {
+  securityNotifyEmail: DevParameter.securityNotifyEmail,
+  tags: { Environment: DevParameter.envName },
 });
-
-// Slack Notifier
-const workspaceId = envVals['slackNotifier']['workspaceId'];
-const channelIdSec = envVals['slackNotifier']['channelIdSec'];
-new BLEAChatbotStack(app, `${pjPrefix}-ChatbotSecurity`, {
-  topicArn: secAlarm.alarmTopic.topicArn,
-  workspaceId: workspaceId,
-  channelId: channelIdSec,
-  env: getProcEnv(),
-});
-
-// --------------------------------- Tagging  -------------------------------------
-
-// Tagging "Environment" tag to all resources in this app
-const envTagName = 'Environment';
-cdk.Tags.of(app).add(envTagName, envVals['envName']);
---
+++ usecases/base-ct-guest/lib/stack/blea-base-ct-guest-stack.ts
@@ -0,0 +1,30 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { SecurityIAM } from '../construct/security-iam';
+import { SecurityLogging } from '../construct/security-logging';
+import { SecurityDetection } from '../construct/security-detection';
+
+export interface BLEABaseCTGuestProps extends StackProps {
+  securityNotifyEmail: string;
+}
+
+export class BLEABaseCTGuestStack extends Stack {
+  constructor(scope: Construct, id: string, props: BLEABaseCTGuestProps) {
+    super(scope, id, props);
+
+    new SecurityIAM(this, 'SecurityIAM');
+
+    // AWS CloudTrail configuration in Control Tower Landing Zone v3.0 will not create CloudWatch Logs LogGroup in each Guest Accounts.
+    // And it will delete these LogGroups when AWS CloudTrial Configuration is disabled in case of updating Landing Zone version from older one.
+    // BLEA should notify their alarms continuously. So, if there is no CloudTrail and CloudWatch Logs in Guest Account, BLEA creates them to notify the Alarms.
+    const securityLogging = new SecurityLogging(this, 'SecurityLogging');
+
+    // Security Alarms
+    // !!! Need to setup SecurityHub, GuardDuty manually on Organizations Management account
+    // AWS Config and CloudTrail are set up by ControlTower
+    new SecurityDetection(this, 'SecurityDetection', {
+      notifyEmail: props.securityNotifyEmail,
+      cloudTrailLogGroupName: securityLogging.trailLogGroup.logGroupName,
+    });
+  }
+}
