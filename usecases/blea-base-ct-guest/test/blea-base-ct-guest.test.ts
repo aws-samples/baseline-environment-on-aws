95cad7e4b50fc9fbf321a00b215e60a1b868d5d6
Kenji Kono <konoken@amazon.co.jp>
2023-01-06 13:37:30 +0900

    test(base-ct-guest): fix snapshot test

--- usecases/blea-base-ct-guest/test/blea-base-ct-guest.test.ts
+++ usecases/blea-base-ct-guest/test/blea-base-ct-guest.test.ts
@@ -1,57 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 
-import { BLEAChatbotStack } from '../lib/blea-chatbot-stack';
+import { BLEABaseCTGuestStack } from '../lib/stack/blea-base-ct-guest-stack';
+import { DevParameter } from '../parameter';
 
-// Load cdk.json to get context parameters
-import * as cdk_json from '../cdk.json';
-import { BLEASecurityAlarmStack } from '../lib/blea-security-alarm-stack';
-import { BLEAConfigRulesStack } from '../lib/blea-config-rules-stack';
-import { BLEAIamStack } from '../lib/blea-iam-stack';
-
-const procEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
-
-const pjPrefix = 'BLEA';
 const app = new cdk.App();
-const envKey = 'dev';
-const envVals = cdk_json['context'][envKey];
 
-describe(`${pjPrefix} ControlTower Stacks`, () => {
-  test('GuestAccount Base Stacks', () => {
-    // ----------------------- Guest Account Base Stacks ------------------------------
-    const configRule = new BLEAConfigRulesStack(app, `${pjPrefix}-ConfigRule`, { env: procEnv });
-    const iam = new BLEAIamStack(app, `${pjPrefix}-Iam`, { env: procEnv });
-
-    // CloudWatch LogGroup Name for CloudTrail - Created by ControlTower for each account
-    const cloudTrailLogGroupName = 'aws-controltower/CloudTrailLogs';
-
-    const secAlarm = new BLEASecurityAlarmStack(app, `${pjPrefix}-SecurityAlarm`, {
-      notifyEmail: envVals['securityNotifyEmail'],
-      cloudTrailLogGroupName: cloudTrailLogGroupName,
-      env: procEnv,
+describe(`BLEABaseCTGuest App`, () => {
+  test('Snapshot test for BLEABaseCTGuest Stack', () => {
+    const bleaBaseCTGuestStack = new BLEABaseCTGuestStack(app, 'DevBLEABaseCTGuest', {
+      securityNotifyEmail: DevParameter.securityNotifyEmail,
+      tags: { Environment: DevParameter.envName },
     });
-
-    // Slack Notifier
-    const workspaceId = envVals['slackNotifier']['workspaceId'];
-    const channelIdSec = envVals['slackNotifier']['channelIdSec'];
-    const chatbotSecurity = new BLEAChatbotStack(app, `${pjPrefix}-ChatbotSecurity`, {
-      topicArn: secAlarm.alarmTopic.topicArn,
-      workspaceId: workspaceId,
-      channelId: channelIdSec,
-      env: procEnv,
-    });
-
-    // Tagging "Environment" tag to all resources in this app
-    const envTagName = 'Environment';
-    cdk.Tags.of(app).add(envTagName, envVals['envName']);
 
     // test with snapshot
-    expect(Template.fromStack(configRule)).toMatchSnapshot();
-    expect(Template.fromStack(iam)).toMatchSnapshot();
-    expect(Template.fromStack(secAlarm)).toMatchSnapshot();
-    expect(Template.fromStack(chatbotSecurity)).toMatchSnapshot();
+    expect(Template.fromStack(bleaBaseCTGuestStack)).toMatchSnapshot();
   });
 });
