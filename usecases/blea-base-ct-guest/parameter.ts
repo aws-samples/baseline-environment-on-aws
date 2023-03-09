b062ebbbe383f8c83dc8a2746ca5d6ad4bf83d30
Kenji Kono <konoken@amazon.co.jp>
2023-03-09 11:33:32 +0900

    add CDK Pipelines into blea-base-ct-guest

--- usecases/blea-base-ct-guest/parameter.ts
+++ usecases/blea-base-ct-guest/parameter.ts
@@ -4,10 +4,19 @@ export interface MyParameter {
   env?: Environment;
   envName: string;
   securityNotifyEmail: string;
+
+  // (optional) AWS CodeStar Connections parameters for CDK Pipelines.
+  // Only used in bin/blea-base-ct-guest-via-cdk-pipelines.ts
+  sourceRepository?: string;
+  sourceBranch?: string;
+  sourceConnectionArn?: string;
 }
 
 // Example
 export const DevParameter: MyParameter = {
   envName: 'Development',
   securityNotifyEmail: 'notify-security@example.com',
+  sourceRepository: 'aws-samples/baseline-environment-on-aws',
+  sourceBranch: 'main',
+  sourceConnectionArn: 'arn:aws:codestar-connections:ap-northeast-1:xxxxxxxxxxxx:connection/example',
 };
