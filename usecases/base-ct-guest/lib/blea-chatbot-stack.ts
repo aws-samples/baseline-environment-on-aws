a3382b457dadd37d34fd0e432c0ab224d2761f66
watany <76135106+watany-dev@users.noreply.github.com>
2023-03-27 09:14:54 +0000

    chatbot to L2 Constructs

--- usecases/base-ct-guest/lib/blea-chatbot-stack.ts
+++ usecases/base-ct-guest/lib/blea-chatbot-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_chatbot as cb } from 'aws-cdk-lib';
 import { aws_iam as iam } from 'aws-cdk-lib';
+import { aws_sns as sns } from 'aws-cdk-lib';
 
 export interface BLEAChatbotStackProps extends cdk.StackProps {
   topicArn: string;
@@ -15,22 +16,15 @@ export class BLEAChatbotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: BLEAChatbotStackProps) {
     super(scope, id, props);
 
-    // AWS Chatbot configuration for sending message
-    const chatbotRole = new iam.Role(this, 'ChatbotRole', {
-      assumedBy: new iam.ServicePrincipal('chatbot.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('ReadOnlyAccess'),
-        iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchReadOnlyAccess'),
-      ],
+    // !!! Create SlackChannel and add aws chatbot app to the room
+    const slackChatbotChannel = new cb.SlackChannelConfiguration(this, 'ChatbotChannel', {
+      slackChannelConfigurationName: `${id}-${props.workspaceId}`,
+      slackChannelId: props.channelId,
+      slackWorkspaceId: props.workspaceId,
     });
 
-    // !!! Create SlackChannel and add aws chatbot app to the room
-    new cb.CfnSlackChannelConfiguration(this, 'ChatbotChannel', {
-      configurationName: `${id}-${props.workspaceId}`,
-      slackChannelId: props.channelId,
-      iamRoleArn: chatbotRole.roleArn,
-      slackWorkspaceId: props.workspaceId,
-      snsTopicArns: [props.topicArn],
-    });
+    // AWS Chatbot configuration for sending message
+    slackChatbotChannel.addNotificationTopic(sns.Topic.fromTopicArn(this, 'ChatbotTopic', props.topicArn));
+    slackChatbotChannel.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('ReadOnlyAccess'));
   }
 }
