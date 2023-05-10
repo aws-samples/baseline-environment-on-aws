e7d0e6c5d7318ecf1beffca83208b75a4cc8d6e0
Yozo Suzuki <suzukyz@amazon.co.jp>
2023-05-10 13:38:39 +0900

    revert test code

--- usecases/blea-gov-base-ct/test/blea-gov-base-ct-via-service-catalog.test.ts
+++ usecases/blea-gov-base-ct/test/blea-gov-base-ct-via-service-catalog.test.ts
@@ -6,14 +6,7 @@ import { BLEAGovBaseCtScStack } from '../lib/stack/blea-gov-base-ct-via-service-
 test('Snapshot test for BLEGovABase Stack', () => {
   const app = new cdk.App();
   const stack = new BLEAGovBaseCtScStack(app, 'Dev-BLEAGovBaseCtSc', {
-    // Account and Region on test
-    //  cdk.process.env.* returns undefined, and cdk.Stack.of(this).* returns ${Token[Region.4]} at test time.
-    //  In such case, RegionInfo.get(cdk.Stack.of(this).region) returns error and test will fail.
-    //  So we pass 'ap-northeast-1' as region code.
-    env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION || 'ap-northeast-1',
-    },
+    env: devParameter.env,
     tags: {
       Repository: 'aws-samples/baseline-environment-on-aws',
       Environment: devParameter.envName,
--- usecases/blea-gov-base-ct/test/blea-gov-base-ct.test.ts
+++ usecases/blea-gov-base-ct/test/blea-gov-base-ct.test.ts
@@ -6,14 +6,7 @@ import { devParameter } from '../parameter';
 test('Snapshot test for BLEGovABaseCt Stack', () => {
   const app = new cdk.App();
   const stack = new BLEAGovBaseCtStack(app, 'Dev-BLEAGovBaseCt', {
-    // Account and Region on test
-    //  cdk.process.env.* returns undefined, and cdk.Stack.of(this).* returns ${Token[Region.4]} at test time.
-    //  In such case, RegionInfo.get(cdk.Stack.of(this).region) returns error and test will fail.
-    //  So we pass 'ap-northeast-1' as region code.
-    env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION || 'ap-northeast-1',
-    },
+    env: devParameter.env,
     tags: {
       Repository: 'aws-samples/baseline-environment-on-aws',
       Environment: devParameter.envName,
