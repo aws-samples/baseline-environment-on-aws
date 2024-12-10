4246e6aa83f367908ff4c6fecaff602d9a757ac0
Kyosuke Konishi <86059523+Konippi@users.noreply.github.com>
2024-12-10 13:24:45 +0900

    feat: migrate from depcheck to knip (#924)
    ## Migrate from `depcheck` to `knip`
    
    The reasons why we should move to `knip` are as follows...
    
    - **Monorepo support**
      - `knip` has built-in support for monorepos and workspaces, while `depcheck` lacks this functionality
    - **Comprehensive analysis**
      - `knip` can analyze not only dependencies but also unused files and exports 
    - **Advanced plugin**
      - `knip` offers a more extensive and advanced plugin system compared to `depcheck`, allowing for better customization and coverage 
    ---
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the [MIT No Attribution (MIT-0)].
    
    [MIT No Attribution (MIT-0)]: https://github.com/aws/mit-0

---
+++ knip.config.ts
@@ -0,0 +1,15 @@
+import { KnipConfig } from 'knip';
+
+export default {
+  ignoreBinaries: ['git-secrets'],
+  ignoreDependencies: ['simple-git-hooks', 'ts-node', 'esbuild'],
+  ignoreExportsUsedInFile: true,
+  workspaces: {
+    'usecases/*': {
+      entry: ['bin/*.ts', 'lambda/**/*.{js,ts}'],
+      project: '**/*.{js,ts}',
+      ignore: ['parameter.ts'], // FIXME: ignore to avoid unused errors on `stagingParameter`. We should use all sample values.
+      ignoreDependencies: ['eslint', 'prettier'],
+    },
+  },
+} as const satisfies KnipConfig;
